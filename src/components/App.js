f29f65e8f294b6811e760facd9b32755a52e4327
vishal pawar <pawarvishal29999@gmail.com>
2023-09-29 11:29:28 +0530

    commit-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,37 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [markdown, setMarkdown] = useState('');
+    const [html, setHtml] = useState('');
+    const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      // You can use a markdown-to-html library here to convert markdown to HTML.
+      // For this example, we'll use a simple method that replaces newlines with <br>.
+      const convertedHtml = markdown.replace(/\n/g, '<br>');
+      setHtml(convertedHtml);
+    }, [markdown]);
+  
+    const handleMarkdownChange = (e) => {
+      setMarkdown(e.target.value);
+    };
+  
+    return (
+      <div className="app">
+        <div className="textarea">
+          <textarea
+            placeholder="Enter markdown task here..."
+            value={markdown}
+           
+            onChange={handleMarkdownChange}
+          />
+        </div>
+        <div className="preview">
+          {loading ? <p className="loading">Loading...</p> : <div dangerouslySetInnerHTML={{ __html: html }} />}
+        </div>
+      </div>
+    );
+  }
+  
+export default App;
